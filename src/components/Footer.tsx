4b176a3dc68a40f8739e2cb1a1bd827a09ba21ed
Maëlle Aucher <maelle.aucher@gmail.com>
2026-01-06 16:15:32 +0100

    chore : footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-light ">
+      <h2 className="text-secondary font-heading text-2xl ">ADAOPTE</h2>
+      <p>
+        Notre mission est de trouver des foyers aimants pour chasue animal
+        abandonnée et de promouvoir le bien-être animal
+      </p>
+
+      <h2 className="text-secondary font-heading text-2xl">INFORMATIONS UTILES</h2>
+      <p>FAQS</p>
+      <p>Conseils d'adoption</p>
+      <p>Nous contacter</p>
+      <p>Mentions légales</p>
+
+      <h2 className="text-secondary font-heading text-2xl">CONTACT</h2>
+      <p>116 Rue du Faubourg Saint-Martin</p>
+      <p>75010 Paris, France</p>
+      <p>Email:contact@adaopte.fr</p>
+      <p>Tél: +33 1 23 45 67 89</p>
+    </footer>
+  );
+}
