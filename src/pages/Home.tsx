b8d020a1c41dece75c8e7af8ea679b864c0041e1
Antoine Mourin <antoinem1pro@gmail.com>
2026-01-08 09:58:35 +0100

    feat create hero component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
-function Home() {
-    return (
-        <div></div>
-    )
-}
+import React from 'react';
+import Hero from '../components/Hero';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  );
+};
 
 export default Home;
