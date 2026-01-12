06b9af22f0d78aa434c56391142976ca0a6b92be
Maëlle Aucher <maelle.aucher@gmail.com>
2026-01-12 11:38:59 +0100

    feat : volunteering card

--- src/pages/Volunteering.tsx
+++ src/pages/Volunteering.tsx
@@ -1,11 +1,16 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 import HeroVolunteering from "../components/HeroVolunteering";
+import BecomeVolunteer from "../components/BecomeVolunteer";
+import ReadyToALife from "../components/ReadyToALife";
+
 function Volunteering() {
   return (
     <>
       <NavBar />
       <HeroVolunteering />
+      <BecomeVolunteer />
+      <ReadyToALife />
       <Footer />
     </>
   );
