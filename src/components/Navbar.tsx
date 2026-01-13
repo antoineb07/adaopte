6805b8015b9c1dd5203ffffd5d8f940610e55b4c
Maëlle Aucher <maelle.aucher@gmail.com>
2026-01-13 10:27:42 +0100

    style : navbar - responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,26 +8,26 @@ export default function NavBar() {
   return (
     <nav className="bg-primary flex gap-y-10 justify-between py-1">
       <div className="scale-50 flex justify-start">
-        <h1 className="text-7xl font-bold">
+        <h1 className="text-2xl md:text-7xl font-bold">
           🐾 Ada<span className="text-btn-secondary">opte</span>
         </h1>
       </div>
       <div className="flex gap-x-6 items-center ">
-        <div className="flex items-center justify-center gap-x-6 pr-80 font-bold text-xl">
+        <div className="flex items-center justify-center gap-x-6 pr-80 font-bold text-xs md:text-xl">
           <Link to="/home" className="flex items-center ">
-            <FiHome className="mr-2 p-0.001 text-2xl " />
+            <FiHome className="mr-2 p-0.001 text-s md:text-2xl " />
             Accueil
           </Link>
           <Link to="/research" className="flex items-center">
-            <LuBone className="mr-3 p-0.001 text-2xl " />
+            <LuBone className="mr-2 p-0.001 text-s md:text-2xl" />
             J'adopte
           </Link>
           <Link to="" className="flex items-center">
-            <LuFish className="mr-2 p-0.001 text-2xl " />
+            <LuFish className="mr-2 p-0.001 text-s md:text-2xl" />
             Guide d'adoption
           </Link>
           <Link to="/volunteering" className="flex items-center">
-            <LuSquirrel className="mr-2 p-0.001 text-2xl " />
+            <LuSquirrel className="mr-2 p-0.001 text-s md:text-2xl" />
             Devenir bénévole
           </Link>
         </div>
